a7d9f301998d3e1329bcbf7697716bbffc28f60f
BLooDBRothER <computertechnology446@gmail.com>
2023-10-19 13:34:43 +0530

    Refactor code with variable name

--- java/leap/src/main/java/Leap.java
+++ java/leap/src/main/java/Leap.java
@@ -1,7 +1,10 @@
 class Leap {
 
+    private final int LEAP_YEAR_DIVISOR = 4;
+    private final int CENTURY_DIVISOR = 100;
+    private final int QUAD_CENTURY_DIVISOR = 400;
     boolean isLeapYear(int year) {
-        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        return year % LEAP_YEAR_DIVISOR == 0 && (year % CENTURY_DIVISOR != 0 || year % QUAD_CENTURY_DIVISOR == 0);
     }
 
 }
